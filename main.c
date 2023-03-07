f877e3bc480985da556b621f76024d48739cdcf0
billie.oconnor@unb.ca <billie.oconnor@unb.ca>
2023-03-07 18:08:08 -0400

    Updated blinks and improved pin definitions Blinks now are at 60, 30, and 15 Hz and pindefs.h definitions of individual pins include the port for clarity of usage.

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include "avr/io.h"
 #include "avr/fuse.h"
+#include "avr/interrupt.h"
+
+#define F_CPU 20000000
+#include "util/delay.h"
+
 #include "pindefs.h"
 
 FUSES = {
@@ -12,12 +17,48 @@ FUSES = {
 	.BOOTEND = FUSE_BOOTEND_DEFAULT,
 };
 
+ISR(USART1_RXC_vect, ISR_NAKED)
+{
+	reti();
+}
+
+ISR(TCA0_CMP0_vect, ISR_NAKED)
+{
+	reti();
+}
+
 int main(void)
 {
+	// Uncomment to allow USART ISR to stomp on PWM ISR
+	// CPUINT.LVL1VEC = USART1_RXC_vect_num;
+
+	// USART is 8N1 asynchronous for testing
+	// Should be 8N1 synchronous for final
+	USART1.CTRLA = USART_RXCIE_bm;
+	USART1.CTRLB = USART_TXEN_bm | USART_RXEN_bm;
+	USART1.CTRLC = USART_CMODE_ASYNCHRONOUS_gc | USART_PMODE_DISABLED_gc | USART_SBMODE_1BIT_gc | USART_CHSIZE_8BIT_gc;
+	USART1.BAUD = 87; // 87 = round(64 * 20 MHz / (16 * 921600 Hz))
+
+	// 60 Hz refresh rate, 20 MHz clock, and 240 dimming steps per period
+	TCA0.SINGLE.PER = 41519; // 240 * floor(20 MHz / (8 * 60 Hz * 240)) - 1
+	TCA0.SINGLE.INTCTRL = TCA_SINGLE_CMP0_bm;
+	TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc | TCA_SINGLE_ENABLE_bm;
+
+	// Columns are active-low, so we need to turn them off first
+	VPORTA.OUT = COL_PORTA_m;
+	VPORTD.OUT = COL_PORTD_m;
+	VPORTF.OUT = COL_PORTF_m;
 	VPORTA.DIR = PORTA_OUTPUT_m;
 	VPORTD.DIR = PORTD_OUTPUT_m;
 	VPORTF.DIR = PORTF_OUTPUT_m;
 	while (1){
-		VPORTA.OUT = VPORTA.OUT ^ (R_PORTA_m);
+		_delay_ms(17);
+		VPORTA.OUT = VPORTA.OUT ^ (R1_PINA_m);
+		_delay_ms(17);
+		VPORTA.OUT = VPORTA.OUT ^ (R1_PINA_m | R2_PINA_m);
+		_delay_ms(17);
+		VPORTA.OUT = VPORTA.OUT ^ (R1_PINA_m);
+		_delay_ms(17);
+		VPORTA.OUT = VPORTA.OUT ^ (R1_PINA_m | R2_PINA_m | R3_PINA_m);
 	}
 }
--- pindefs.h
+++ pindefs.h
@@ -2,41 +2,41 @@
 #define _PINDEFS_H_
 
 // PORTA definitions
-#define G1_PIN_m (1 << 7)
-#define COL7_PIN_m (1 << 6)
-#define R1_PIN_m (1 << 4)
-#define R2_PIN_m (1 << 3)
-#define R3_PIN_m (1 << 2)
-#define COL2_PIN_m (1 << 1)
-#define COL1_PIN_m (1 << 0)
+#define G1_PINA_m (1 << 7)
+#define COL7_PINA_m (1 << 6)
+#define R1_PINA_m (1 << 4)
+#define R2_PINA_m (1 << 3)
+#define R3_PINA_m (1 << 2)
+#define COL2_PINA_m (1 << 1)
+#define COL1_PINA_m (1 << 0)
 
-#define R_PORTA_m (R1_PIN_m | R2_PIN_m | R3_PIN_m)
-#define COL_PORTA_m (COL7_PIN_m | COL2_PIN_m | COL1_PIN_m)
-#define G_PORTA_m (G1_PIN_m)
+#define R_PORTA_m (R1_PINA_m | R2_PINA_m | R3_PINA_m)
+#define COL_PORTA_m (COL7_PINA_m | COL2_PINA_m | COL1_PINA_m)
+#define G_PORTA_m (G1_PINA_m)
 
 #define PORTA_OUTPUT_m (R_PORTA_m | COL_PORTA_m | G_PORTA_m)
 
 // PORTD definitions
-#define COL6_PIN_m (1 << 7)
-#define COL5_PIN_m (1 << 6)
-#define COL8_PIN_m (1 << 5)
-#define B3_PIN_m (1 << 4)
-#define B2_PIN_m (1 << 3)
-#define B1_PIN_m (1 << 2)
-#define G3_PIN_m (1 << 1)
-#define G2_PIN_m (1 << 0)
+#define COL6_PIND_m (1 << 7)
+#define COL5_PIND_m (1 << 6)
+#define COL8_PIND_m (1 << 5)
+#define B3_PIND_m (1 << 4)
+#define B2_PIND_m (1 << 3)
+#define B1_PIND_m (1 << 2)
+#define G3_PIND_m (1 << 1)
+#define G2_PIND_m (1 << 0)
 
-#define COL_PORTD_m (COL6_PIN_m | COL5_PIN_m | COL8_PIN_m)
-#define B_PORTD_m (B3_PIN_m | B2_PIN_m | B1_PIN_m)
-#define G_PORTD_m (G3_PIN_m | G2_PIN_m)
+#define COL_PORTD_m (COL6_PIND_m | COL5_PIND_m | COL8_PIND_m)
+#define B_PORTD_m (B3_PIND_m | B2_PIND_m | B1_PIND_m)
+#define G_PORTD_m (G3_PIND_m | G2_PIND_m)
 
 #define PORTD_OUTPUT_m (COL_PORTD_m | B_PORTD_m | G_PORTD_m)
 
 // PORTF definitions
-#define COL4_PIN_m (1 << 4)
-#define COL3_PIN_m (1 << 0)
+#define COL4_PINF_m (1 << 4)
+#define COL3_PINF_m (1 << 0)
 
-#define COL_PORTF_m (COL4_PIN_m | COL3_PIN_m)
+#define COL_PORTF_m (COL4_PINF_m | COL3_PINF_m)
 
 #define PORTF_OUTPUT_m (COL_PORTF_m)
 
